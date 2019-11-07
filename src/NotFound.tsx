75670dd0b313869228483b1922c2db866de223b6
ryota-murakami <dojce1048@gmail.com>
2019-11-07 23:28:13 +0900

    NotFound: change form

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
 
-export const NotFound = (props: RouteComponentProps) => (
+const NotFound: React.FC<RouteComponentProps> = props => (
   <div data-cy="not-found-page" style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }} /* eslint-disable-line prettier/prettier */>
     <h1>Page Not Found</h1>
   </div>
 )
+
+export default NotFound
