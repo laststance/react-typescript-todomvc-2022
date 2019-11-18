a4065871c9768dbf25f56b29959d2c949b807ecf
ryota-murakami <dojce1048@gmail.com>
2019-11-18 23:19:10 +0900

    ErrorBoundary.test: refactor along with  RTL bestpractice

--- src/ErrorBoundary.test.js
+++ src/ErrorBoundary.test.js
@@ -6,13 +6,10 @@ import '@testing-library/jest-dom/extend-expect'
 
 test('shlould render fallback page when react take undefined as a node', () => {
   const WrongComponent = () => undefined
-  const { getByTestId } = render(
+  const { getByText } = render(
     <ErrorBoundary>
       <WrongComponent />
     </ErrorBoundary>
   )
-  expect(getByTestId('error-boundary-fallback-component')).not.toBeNull()
-  expect(getByTestId('error-boundary-fallback-component')).toHaveTextContent(
-    'Something Error Ooccurring'
-  )
+  expect(getByText('Something Error Ooccurring')).toBeInTheDocument()
 })
