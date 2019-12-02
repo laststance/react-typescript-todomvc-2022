b945dd03b9231c04a07deda402d7a5ef430531b3
ryota-murakami <dojce1048@gmail.com>
2019-12-02 21:11:48 +0900

    fix lint

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,6 +14,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const percyHealthCheck = require('@percy/cypress/task')
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
