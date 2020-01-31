6ce6fb72524f2c1dc4ecc111e4a02b636f04ad9b
ryota-murakami <dojce1048@gmail.com>
2020-02-01 01:38:33 +0900

    percy: add screenshot scene

--- cypress/integration/add_todo.spec.js
+++ cypress/integration/add_todo.spec.js
@@ -1,6 +1,7 @@
 context('Add Todo', () => {
   it('can add todo', () => {
     cy.visit('http://localhost:3000/')
+    cy.percySnapshot('Fresh user first-view')
 
     // can type text and submit
     cy.get('[data-cy=new-todo-input-text]')
