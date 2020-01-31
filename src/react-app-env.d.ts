27d8a17d4ac6d7a4a202c58439886efa2f14b158
ryota-murakami <dojce1048@gmail.com>
2020-02-01 01:59:19 +0900

    prettier

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -42,9 +42,9 @@ declare module '*.webp' {
 declare module '*.svg' {
   import * as React from 'react'
 
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement>
-  >
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<
+    SVGSVGElement
+  >>
 
   const src: string
   export default src
