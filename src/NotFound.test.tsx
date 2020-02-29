48b0adb3a588138bcce2f62d9df328eb8748c9e4
ryota-murakami <dojce1048@gmail.com>
2020-03-01 02:01:38 +0900

    NotFound.test.tsx: fix mistakeing setting

--- src/NotFound.test.tsx
+++ src/NotFound.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import { NotFound } from './NotFound'
 
 test('<NotFound /> should render Page Not Found message', () => {
-  const { getAllByText } = render(<NotFound />)
-  expect(getAllByText('Page Not Found')).toBeInTheDocument()
+  const { getByText } = render(<NotFound />)
+  expect(getByText('Page Not Found')).toBeInTheDocument()
 })
