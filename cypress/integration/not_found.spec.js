d51c026d9de5af180dc3864ce9fcb803ab00ecd8
ryota-murakami <dojce1048@gmail.com>
2020-03-19 02:13:00 +0900

    percy: add Not Found

--- cypress/integration/not_found.spec.js
+++ cypress/integration/not_found.spec.js
@@ -3,6 +3,7 @@ context('Routing', () => {
     // no exist uri
     cy.visit('http://localhost:3000/jiawojefpwielj0rijfpopo')
 
+    cy.percySnapshot('Not Found')
     cy.get('[data-cy=not-found-page]')
       .should('exist')
       .should('contain', 'Page Not Found')
