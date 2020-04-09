75e5c685873d3dcfff169719aaabe82ce015b3a3
ryota-murakami <dojce1048@gmail.com>
2020-04-10 07:53:09 +0900

    prettier

--- src/App/TodoTextInput/index.tsx
+++ src/App/TodoTextInput/index.tsx
@@ -11,6 +11,7 @@ const TodoTextInput: React.FC = () => {
   function addTodo(e: React.KeyboardEvent<HTMLInputElement>): void {
     if (textInput.current === null) return
     if (e.key === 'Enter' && textInput.current.value.trim().length > 0) {
+      /* eslint-disable-line prettier/prettier */
 
       // make new TODO object
       const todo: Todo = {
