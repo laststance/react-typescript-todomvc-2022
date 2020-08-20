f58bf86742e62a4ceeee898171555ec9860ad92f
ryota-murakami <dojce1048@gmail.com>
2020-08-21 01:01:03 +0900

    test: use screen api

--- src/App/TodoList/index.test.tsx
+++ src/App/TodoList/index.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import Provider from '@laststance/use-app-state'
-import '@testing-library/jest-dom'
 import TodoList from './index'
 import { AppState } from '../../index'
+import '@testing-library/jest-dom'
 
 const initialAppState: AppState = {
   todoList: [
@@ -26,54 +26,54 @@ const initialAppState: AppState = {
 }
 
 test('should be render 3 todo items in initialAppState', () => {
-  const { getByTestId, getAllByTestId } = render(
+  render(
     <Provider initialState={initialAppState}>
       <TodoList path="/" />
     </Provider>
   )
 
-  expect(getByTestId('todo-list')).toBeInTheDocument()
-  expect(getByTestId('todo-list').children.length).toBe(3)
-  expect(Array.isArray(getAllByTestId('todo-item'))).toBe(true)
-  expect(getAllByTestId('todo-item')[0]).toHaveTextContent('monster')
-  expect(getAllByTestId('todo-item')[1]).toHaveTextContent('boss black')
-  expect(getAllByTestId('todo-item')[2]).toHaveTextContent('caffe latte')
+  expect(screen.getByTestId('todo-list')).toBeInTheDocument()
+  expect(screen.getByTestId('todo-list').children.length).toBe(3)
+  expect(Array.isArray(screen.getAllByTestId('todo-item'))).toBe(true)
+  expect(screen.getAllByTestId('todo-item')[0]).toHaveTextContent('monster')
+  expect(screen.getAllByTestId('todo-item')[1]).toHaveTextContent('boss black')
+  expect(screen.getAllByTestId('todo-item')[2]).toHaveTextContent('caffe latte')
 })
 
 test('should be work delete todo button', () => {
-  const { getByTestId, getAllByTestId } = render(
+  render(
     <Provider initialState={initialAppState}>
       <TodoList path="/" />
     </Provider>
   )
 
   // delete first item
-  fireEvent.click(getAllByTestId('delete-todo-btn')[0])
+  fireEvent.click(screen.getAllByTestId('delete-todo-btn')[0])
   // assertions
-  expect(getByTestId('todo-list').children.length).toBe(2)
-  expect(Array.isArray(getAllByTestId('todo-item'))).toBe(true)
-  expect(getAllByTestId('todo-item')[0]).toHaveTextContent('boss black')
-  expect(getAllByTestId('todo-item')[1]).toHaveTextContent('caffe latte')
+  expect(screen.getByTestId('todo-list').children.length).toBe(2)
+  expect(Array.isArray(screen.getAllByTestId('todo-item'))).toBe(true)
+  expect(screen.getAllByTestId('todo-item')[0]).toHaveTextContent('boss black')
+  expect(screen.getAllByTestId('todo-item')[1]).toHaveTextContent('caffe latte')
 })
 
 test('should be work correctly all completed:true|false checkbox toggle button', () => {
-  const { getByTestId, getAllByTestId } = render(
+  render(
     <Provider initialState={initialAppState}>
       <TodoList path="/" />
     </Provider>
   )
 
   // toggle on
-  fireEvent.click(getByTestId('toggle-all-btn'))
+  fireEvent.click(screen.getByTestId('toggle-all-btn'))
   // should be completed all todo items
-  expect((getAllByTestId('todo-item-complete-check')[0] as HTMLInputElement).checked).toBe(true) /* eslint-disable-line prettier/prettier */
-  expect((getAllByTestId('todo-item-complete-check')[1] as HTMLInputElement).checked).toBe(true) /* eslint-disable-line prettier/prettier */
-  expect((getAllByTestId('todo-item-complete-check')[2] as HTMLInputElement).checked).toBe(true) /* eslint-disable-line prettier/prettier */
+  expect((screen.getAllByTestId('todo-item-complete-check')[0] as HTMLInputElement).checked).toBe(true) /* eslint-disable-line prettier/prettier */
+  expect((screen.getAllByTestId('todo-item-complete-check')[1] as HTMLInputElement).checked).toBe(true) /* eslint-disable-line prettier/prettier */
+  expect((screen.getAllByTestId('todo-item-complete-check')[2] as HTMLInputElement).checked).toBe(true) /* eslint-disable-line prettier/prettier */
 
   // toggle off
-  fireEvent.click(getByTestId('toggle-all-btn'))
+  fireEvent.click(screen.getByTestId('toggle-all-btn'))
   // should be not comleted all todo items
-  expect((getAllByTestId('todo-item-complete-check')[0] as HTMLInputElement).checked).toBe(false) /* eslint-disable-line prettier/prettier */
-  expect((getAllByTestId('todo-item-complete-check')[1] as HTMLInputElement).checked).toBe(false) /* eslint-disable-line prettier/prettier */
-  expect((getAllByTestId('todo-item-complete-check')[2] as HTMLInputElement).checked).toBe(false) /* eslint-disable-line prettier/prettier */
+  expect((screen.getAllByTestId('todo-item-complete-check')[0] as HTMLInputElement).checked).toBe(false) /* eslint-disable-line prettier/prettier */
+  expect((screen.getAllByTestId('todo-item-complete-check')[1] as HTMLInputElement).checked).toBe(false) /* eslint-disable-line prettier/prettier */
+  expect((screen.getAllByTestId('todo-item-complete-check')[2] as HTMLInputElement).checked).toBe(false) /* eslint-disable-line prettier/prettier */
 })
--- src/ErrorBoundary.test.js
+++ src/ErrorBoundary.test.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import ErrorBoundary from './ErrorBoundary'
 
 import '@testing-library/jest-dom'
 
 test('should be render fallback page Error was thrown', () => {
   const InvalidComponent = () => undefined
-  const { getByText } = render(
+  render(
     <ErrorBoundary>
       <InvalidComponent />
     </ErrorBoundary>
   )
-  expect(getByText('Something Error Ooccurring')).toBeInTheDocument()
+  expect(screen.getByText('Something Error Ooccurring')).toBeInTheDocument()
 })
