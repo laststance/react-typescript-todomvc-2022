843982f36f5515f6844ebabf15149195a76cc67e
ryota-murakami <dojce1048@gmail.com>
2020-09-14 03:25:52 +0900

    fix #288

--- cypress/integration/mobile_realworld_usecase.js
+++ cypress/integration/mobile_realworld_usecase.js
@@ -37,13 +37,13 @@ context('Mobile Real World Usecase', () => {
       .should('not.have.checked')
 
     // can all task checked as completed by click toggle all button
-    cy.get('[data-cy=toggle-all-btn]').click()
+    cy.get('[data-cy=toggle-all-btn]').click({ force: true }) // { force: true } reason @see https://github.com/laststance/create-react-app-typescript-todo-example-2020/issues/288
     cy.get('[data-cy=remaining-uncompleted-todo-count]').should('contain', '0')
     // there is no side-effect to todo items value by toggle all button
     cy.get('[data-cy=todo-item]:first-of-type').should('have.text', 'three')
     cy.get('[data-cy=todo-item]:nth-of-type(2)').should('have.text', 'two')
     // can reverse todo state to un-completed after click toggle all button again
-    cy.get('[data-cy=toggle-all-btn]').click()
+    cy.get('[data-cy=toggle-all-btn]').click({ force: true }) // { force: true } reason @see https://github.com/laststance/create-react-app-typescript-todo-example-2020/issues/288
     cy.get('[data-cy=remaining-uncompleted-todo-count]').should('contain', '3')
     // [state: 3 todo, 0 completed] it doesn't "Clear cmpleted" button on footer
     cy.get('[data-cy=clear-completed-button]').should('not.visible')
