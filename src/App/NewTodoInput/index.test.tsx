e88f5e04fefaa94b757263710437a04b64d2fcec
ryota-murakami <dojce1048@gmail.com>
2020-10-02 07:33:17 +0900

    src/App/NewTodoInput/index.test.tsx

--- src/App/NewTodoInput/index.test.tsx
+++ src/App/NewTodoInput/index.test.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import { RecoilRoot } from 'recoil'
 import NewTodoTextInput from './index'
-import { InjectTestingRecoilState } from '../../testUtils'
+import { renderWithReciolRoot } from '../../testUtils'
 
 test('should be render <TodoTextInput/>', () => {
-  const screen = render(
-    <RecoilRoot>
-      <InjectTestingRecoilState />
-      <NewTodoTextInput />
-    </RecoilRoot>
-  )
+  const screen = renderWithReciolRoot(<NewTodoTextInput />)
 
   // Header text
   expect(screen.getByText('todos')).toBeInTheDocument()
