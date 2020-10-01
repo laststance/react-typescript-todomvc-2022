dbf029f4e7322c1fb20574142d5cbf11c2e5e9c4
ryota-murakami <dojce1048@gmail.com>
2020-10-02 07:31:43 +0900

    update  src/App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,42 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { RecoilRoot } from 'recoil'
-import { InjectTestingRecoilState } from './testUtils'
+import ReactDOM from 'react-dom'
 import App from './App'
+import { recoilState } from './dataStructure'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
+
+  const todo: AppState = {
+    todoList: [
+      {
+        id: 'TsHx9eEN5Y4A',
+        bodyText: 'monster',
+        completed: false,
+      },
+      {
+        id: 'ba91OwrK0Dt8',
+        bodyText: 'boss black',
+        completed: false,
+      },
+      {
+        id: 'QwejYipEf5nk',
+        bodyText: 'caffe latte',
+        completed: false,
+      },
+    ],
+  }
+
   ReactDOM.render(
-    <RecoilRoot>
-      <InjectTestingRecoilState />
+    <RecoilRoot
+      initializeState={({ set }) => {
+        set(recoilState, todo)
+      }}
+    >
       <App path="/" />
     </RecoilRoot>,
     div
   )
+
   ReactDOM.unmountComponentAtNode(div)
 })
