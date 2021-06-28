e91f7779a82ff31721284480e3c061bf5dd209ad
ryota-murakami <dojce1048@gmail.com>
2021-06-29 00:13:07 +0900

    upgrade cypress

--- src/testUtil.tsx
+++ src/testUtil.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { mount } from '@cypress/react'
 import { AppState, recoilState } from './dataStructure'
 import { RenderResult } from '@testing-library/react'
 import { MutableSnapshot, RecoilRoot } from 'recoil'
@@ -14,21 +13,6 @@ export const renderWithRecoilRoot = (
   initialRecoilStateValue: AppState = defaultValue
 ): RenderResult =>
   render(
-    <RecoilRoot
-      initializeState={({ set }: MutableSnapshot): void =>
-        set(recoilState, initialRecoilStateValue)
-      }
-    >
-      {ui}
-    </RecoilRoot>
-  )
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const mountCypressWithRecoilRoot = (
-  ui: React.ReactElement,
-  initialRecoilStateValue: AppState = defaultValue
-) /* Chainable<JQuery<any>> output: TS2304: Cannot find name 'Chainable' */ =>
-  mount(
     <RecoilRoot
       initializeState={({ set }: MutableSnapshot): void =>
         set(recoilState, initialRecoilStateValue)
