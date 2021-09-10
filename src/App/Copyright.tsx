1f15f9a04ac43555cacae172023d238328b0b7fa
ryota-murakami <dojce1048@gmail.com>
2021-09-10 21:46:44 +0900

    memoized Copyright

--- src/App/Copyright.tsx
+++ src/App/Copyright.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { memo } from 'react'
 
-const Copyright: React.FC = () => (
-  <footer className="info">
-    <p>
-      Created by <a href="https://ryota-murakami.github.io/">Ryota Murakamai</a>
-    </p>
-    <p>
-      Part of <a href="http://todomvc.com">TodoMVC</a>
-    </p>
-  </footer>
+const Copyright: React.FC = memo(
+  () => (
+    <footer className="info">
+      <p>
+        Created by{' '}
+        <a href="https://ryota-murakami.github.io/">Ryota Murakamai</a>
+      </p>
+      <p>
+        Part of <a href="http://todomvc.com">TodoMVC</a>
+      </p>
+    </footer>
+  ),
+  () => true
 )
 
 export default Copyright
