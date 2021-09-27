e0ba05d59696b0da19945ba1a84bfc0c7ae2a24f
ryota-murakami <dojce1048@gmail.com>
2021-09-27 14:31:28 +0900

    fix .eslintrc json syntax

---
+++ cypress/component/NewTodoInput.component.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import NewTodoTextInput from '../../src/App/NewTodoInput'
+import { Layout } from '../../src/App/style'
+import { mountCypressWithRecoilRoot } from '../../src/testUtil'
+
+it('Translate react-testing-library from src/App/NewTodoInput/index.test.tsx', () => {
+  mountCypressWithRecoilRoot(
+    <Layout>
+      <NewTodoTextInput />
+    </Layout>
+  )
+  const input = cy.findByTestId('new-todo-input-text')
+  // Header text
+  cy.findByText('todos').should('exist')
+
+  // Placeholder
+  cy.findByPlaceholderText('What needs to be done?').should('exist')
+
+  // type 'tidying my room' then submit
+  input
+    .type('tidying my room')
+    .should('have.value', 'tidying my room')
+    .type('{enter}')
+    .should('not.have.value')
+})
