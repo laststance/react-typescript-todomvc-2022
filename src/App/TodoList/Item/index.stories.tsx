254505e46634d510c4a0e1b2c09997baf3838ffb
seito.tanaka <seito.tanaka.dev@gmail.com>
2022-05-06 11:03:17 +0900

    Add item editing story

--- src/App/TodoList/Item/index.stories.tsx
+++ src/App/TodoList/Item/index.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { userEvent, within } from '@storybook/testing-library'
 import Item from "."
 
 export default {
@@ -6,8 +7,24 @@ export default {
   component: Item,
 }  as ComponentMeta<typeof Item>
 
-export const Default: ComponentStory<typeof Item> = () =>
-  <Item todo={{ id: '1', completed: false, bodyText: 'Test task'}} />
+const Template: ComponentStory<typeof Item> = (args) => <Item {...args} />
 
-export const Completed: ComponentStory<typeof Item> = () =>
-  <Item todo={{ id: '1', completed: true, bodyText: 'Test task'}} />
+export const Default = Template.bind({});
+Default.args = {
+  todo: { id: '1', completed: false, bodyText: 'Test task' }
+}
+
+export const Editing = Template.bind({});
+Editing.args = {
+  todo: { id: '1', completed: false, bodyText: 'Test task' }
+}
+Editing.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.click(canvas.getByTestId('todo-body-text'));
+}
+
+export const Completed = Template.bind({});
+Completed.args = {
+  todo: { id: '1', completed: true, bodyText: 'Test task' }
+}
