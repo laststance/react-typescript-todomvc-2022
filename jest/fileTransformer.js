9c0417aa375bdbfb6e44fee9cf26e0784f78990a
likui628 <likui.it@gmail.com>
2022-06-22 19:14:56 +0800

    configure jest

---
+++ jest/fileTransformer.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  process(_sourceText, sourcePath, _options) {
+    return {
+      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
+    };
+  },
+};
