4282e0c89bd5041ce6c8c2033e496c0aaa53175e
ryota-murakami <dojce1048@gmail.com>
2022-06-23 21:00:27 +0900

    delete ServiceWorker because that depends on CRA env val

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import './index.css'
 import App from './App'
 import ErrorBoundary from './ErrorBoundary'
 import { NotFound } from './NotFound'
-import * as serviceWorker from './serviceWorkerRegistration'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
@@ -22,8 +21,3 @@ root.render(
     </RecoilRoot>
   </ErrorBoundary>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
